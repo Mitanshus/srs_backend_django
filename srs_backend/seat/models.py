9a3b4d8c22ad3b98515f222f262c1902aed2adf3
surajgate <surajgate73@gmail.com>
2023-09-27 15:04:16 +0530

    seat model

--- srs_backend/seat/models.py
+++ srs_backend/seat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+import uuid
+from cabin.models import Cabin
 # Create your models here.
+
+
+class Seat(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False,
+                          auto_created=True, default=uuid.uuid4)
+    status = models.enums()
+    cabin_id = models.ForeignKey(Cabin, on_delete=models.CASCADE, null=False)
+    code = models.CharField(max_length=50, null=False)
+
+    def __str__(self) -> str:
+        return self.code
