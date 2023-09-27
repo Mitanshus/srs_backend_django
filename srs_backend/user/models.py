e3547a6e536c9e65e069bf88414f76f712fd96a4
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-27 10:27:17 +0530

    user model

--- srs_backend/user/models.py
+++ srs_backend/user/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id:models.UUIDField(primary_key=True,auto_created=True,editable=False,default=uuid.uuid4)
+    first_name:models.CharField(null=False)
+    last_name:models.CharField(null=False)
+    email:models.EmailField(null=False,unique=True,max_length=254)
+    password:models.CharField(null=False)
+    primary_location:models.
