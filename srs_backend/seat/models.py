a4616da5625d4bcf373c72fc026acbaf1657aaf3
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-27 15:09:35 +0530

    SEAT MODEL ENUM ADDED

--- srs_backend/seat/models.py
+++ srs_backend/seat/models.py
@@ -2,12 +2,15 @@ from django.db import models
 import uuid
 from cabin.models import Cabin
 # Create your models here.
-
+class Status(models.TextChoices):
+    AVAILABLE='AVAILABLE'
+    RESERVATED='APPROVED'
+    DELETED = 'DELETED'
 
 class Seat(models.Model):
     id = models.UUIDField(primary_key=True, editable=False,
                           auto_created=True, default=uuid.uuid4)
-    status = models.enums()
+    status = models.CharField(choices=Status.choices,default="AVAILABLE")
     cabin_id = models.ForeignKey(Cabin, on_delete=models.CASCADE, null=False)
     code = models.CharField(max_length=50, null=False)
 
