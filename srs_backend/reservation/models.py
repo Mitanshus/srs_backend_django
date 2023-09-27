cbc8ead42f59cabe0543bd76fce4e798f5f4839c
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-27 14:22:28 +0530

    models added in url

--- srs_backend/reservation/models.py
+++ srs_backend/reservation/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class BookingStatus(models.TextChoices):
+    BOOKED = 'BOOKED'
+    CANCELLED = 'CANCELLED' 
+    OCCUPIED = 'OCCUPIED'
+    PENDING = 'PENDING'
+
+class ApprovalStatus(models.TextChoices):
+    PENDING='PENDING'
+    APPROVED='APPROVED'
+    REJECTED = 'REJECTED'
+
+class Reservations(models.Model):
+    id:models.UUIDField(default=uuid.uuid4,primary_key=True,auto_created=True)
+    user_id:models.ForeignKey(to=User,on_delete=models.CASCADE)
+    seat_id:models.ForeignKey(to="",on_delete=models.CASCADE)
+    reservation_start_date:models.DateField(null=False)
+    reservation_end_date:models.DateField(null=False)
+    status:models.CharField(max_length= 10,choices=BookingStatus.choices)
+    approval_status:models.CharField(max_length=10,choices=ApprovalStatus.choices)
