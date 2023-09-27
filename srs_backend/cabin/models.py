470bf2a222ceed2363608fd05c75fe0dd24bbeec
surajgate <surajgate73@gmail.com>
2023-09-27 14:20:18 +0530

    seat

--- srs_backend/cabin/models.py
+++ srs_backend/cabin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import uuid
+from location.models import Locations
 # Create your models here.
+
+
+class Cabin (models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True,
+                          editable=False, default=uuid.uuid4)
+    location_id = models.ForeignKey(
+        Locations, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=50, null=False)
+    code = models.CharField(max_length=50, null=False)
+
+    def __str__(self) -> str:
+        return self.name
--- srs_backend/reservation/models.py
+++ srs_backend/reservation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+import uuid
+from user.models import User
+from seat.models import Seat
 # Create your models here.
+
+
+class Reservation (models.Model):
+    id = models.UUIDField(primary_key=True, auto_created=True,
+                          editable=False, default=uuid.uuid4)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    seat_id = models.ForeignKey(Seat, on_delete=models.CASCADE, null=False)
+    reservation_start_date = models.DateField(null=False)
+    reservation_end_date = models.DateField(null=False)
+    status = models.enums()
+    approval_status = models.enums()
