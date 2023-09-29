812f8a78bdf74b2f30cc267ce42c180052075351
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-29 13:13:02 +0530

    added first and last name

--- srs_backend/user_schedule/serializer.py
+++ srs_backend/user_schedule/serializer.py
@@ -3,10 +3,16 @@ from user_schedule.models import UserSchedule
 
 
 class user_schedule_serializer (serializers.ModelSerializer):
+
+    user_name = serializers.SerializerMethodField()
+
     class Meta:
         model = UserSchedule
-        fields = '__all__'
+        fields = ['__all__','user_name']
         read_only_fields = 'id',
 
     def create(self, validated_data):
         return UserSchedule.objects.create(**validated_data)
+    
+    def get_user_name(self, obj):
+        return f"{obj.user_id.first_name} {obj.user_id.last_name}"
