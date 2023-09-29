8043945c3ede863c3b28da3c929fa2f6573185dc
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-29 14:01:33 +0530

    seprated last and first name

--- srs_backend/user_schedule/serializer.py
+++ srs_backend/user_schedule/serializer.py
@@ -4,15 +4,21 @@ from user_schedule.models import UserSchedule
 
 class user_schedule_serializer (serializers.ModelSerializer):
 
-    user_name = serializers.SerializerMethodField()
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+
 
     class Meta:
         model = UserSchedule
-        fields = ['id','user_id','company_id','monday','tuesday','wednesday','thursday','saturday','sunday','user_name']
+        fields = ['id','user_id','company_id','monday','tuesday','wednesday','thursday','saturday','sunday','first_name','last_name']
         read_only_fields = 'id',
 
     def create(self, validated_data):
         return UserSchedule.objects.create(**validated_data)
     
-    def get_user_name(self, obj):
-        return f"{obj.user_id.first_name} {obj.user_id.last_name}"
+    def get_first_name(self, obj):
+        return f"{obj.user_id.first_name}"
+    
+    def get_last_name(self, obj):
+        return f"{obj.user_id.last_name}"
+
