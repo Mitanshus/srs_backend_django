41f995c72c670cf779eee73d1409e6dd2843e349
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-29 18:30:56 +0530

    model serilizer updated

--- srs_backend/location/models.py
+++ srs_backend/location/models.py
@@ -7,18 +7,20 @@ class Locations(models.Model):
     name=models.CharField(max_length=50)
     code=models.CharField(max_length=50)
     company_id=models.ForeignKey(to=Company,on_delete=models.CASCADE)
-    min_days=models.IntegerField(null=False)
-    max_days=models.IntegerField(null=False)
-    max_pre_booking_time=models.IntegerField(null=False)
-    max_duration=models.IntegerField(null=False)
+    min_days=models.IntegerField(null=True)
+    max_days=models.IntegerField(null=True)
+    max_pre_booking_time=models.IntegerField(null=True)
+    max_duration=models.IntegerField(null=True),
+    is_schedule_restricted=models.BooleanField(null=True,)
     def serialize(self):
         return {
-            'id': self.id,
+            'id': str(self.id),
             'name': self.name,
             'code': self.code,
             'company_id': self.company_id.name,
-            'min_days': self.min_days,
-            'max_days':self.max_days,
-            'max_pre_booking_time':self.max_pre_booking_time,
-            'max_duration':self.max_duration            
+            'min_days': str(self.min_days),
+            'max_days':str(self.max_days),
+            'max_pre_booking_time':str(self.max_pre_booking_time),
+            'max_duration':str(self.max_duration)  ,
+            'is_schedule_restricted':str(self.is_schedule_restricted)          
         }
