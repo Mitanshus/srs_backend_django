0382ce6d2659b33622a1a129b634244b39eb19bd
Mitanshus <117567772+Mitanshus@users.noreply.github.com>
2023-09-29 13:03:10 +0530

    available seats half done

--- srs_backend/reservation/views.py
+++ srs_backend/reservation/views.py
@@ -40,66 +40,74 @@ class delete_reservation_view(APIView):
             return Response({"message": "Reservation does not exist !"})
         
 class AvailableSeatsView(APIView):
-    def get(self, request, company_id, location_id, start_date, end_date):
-        try:
-            # Find the company
-            existing_company = Company.objects.filter(id=company_id).first()
-            if not existing_company:
-                return Response({'message': 'Company not found'}, status=404)
 
-            # Find the location
-            existing_location = Locations.objects.filter(id=location_id).first()
-            if not existing_location:
-                return Response({'message': 'Location not found'}, status=404)
+  def get(self, request, company_id, location_id, start_date, end_date):
+  
+    try:
+    
+      start_datetime = datetime.strptime(start_date, "%Y-%m-%d")  
+      end_datetime = datetime.strptime(end_date, "%Y-%m-%d")
+      
+      company = Company.objects.get(id=company_id)
+      
+      # Get all cabins for this location
+      cabins = Cabin.objects.filter(location_id=location_id)  
+      
+      # Get list of locations from cabins
+      locations = [cabin.location_id for cabin in cabins]  
+      
+      booked_reservations = Reservations.objects.filter(
+        Q(reservation_start_date__lt=end_datetime, reservation_end_date__gte=start_datetime) | 
+        Q(reservation_start_date__lte=start_datetime, reservation_end_date__gte=end_datetime),
+        status='BOOKED'
+      ).values_list('seat_id', flat=True)
 
-            current_time = datetime.now()
+      available_seats = Seat.objects.filter(cabin__in=cabins).values()
+      
+      cabins_with_seats = {}
+      for seat in available_seats:
+            
+        cabin = cabins.get(id=seat['cabin_id'])
+            
+        if seat['cabin_id'] not in cabins_with_seats:
+            
+          cabins_with_seats[seat['cabin_id']] = {
+            "Cabin": {
+              "id": cabin.id,
+              "location_id": cabin.location_id,
+              "name": cabin.name,  
+              "code": cabin.code
+            },
+            "Seats": []
+          }
+          
+        is_partially_booked = Reservations.objects.filter(
+          seat_id=seat['id'],
+          reservation_start_date__lt=end_datetime,
+          reservation_end_date__gt=start_datetime
+        ).exists()
 
-            if start_date < current_time or end_date < current_time:
-                return Response({'message': 'Cannot select a previous time for booking'}, status=400)
+        is_reserved = seat['status'] == 'RESERVED'
+        
+        cabins_with_seats[seat['cabin_id']]['Seats'].append({
+          "id": seat['id'],
+          "status": seat['status'],
+          "cabin_id": seat['cabin_id'], 
+          "code": seat['code'],
+          "isBooked": is_partially_booked,
+          "isReserved": is_reserved 
+        })
 
-            if start_date > end_date:
-                return Response({'message': 'End Date & time cannot be prior to start date & time'}, status=400)
+      result = list(cabins_with_seats.values())
+      
+      return Response({
+        "message": "Available seats fetched successfully",
+        "data": result  
+      })
 
-            booked_reservations = Reservations.objects.filter(
-                Q(reservation_start_date__lt=end_date, reservation_end_date__gte=start_date) |
-                Q(reservation_start_date__lte=start_date, reservation_end_date__gte=end_date) |
-                Q(reservation_start_date__lte=start_date, reservation_end_date__gte=end_date),
-                status='BOOKED'
-            ).values_list('seat_id', flat=True)
+    except Exception as e:
+      return Response({"error": str(e)}, status=500)
 
-            available_seats = Seat.objects.filter(
-                cabin__location_id=location_id
-            ).select_related('cabin').values('cabin__id', 'cabin__location_id', 'cabin__name', 'cabin__code', 'id', 'status', 'cabin_id', 'code')
-
-            cabins_with_seats = {}
-            for seat in available_seats:
-                cabin_id = seat['cabin__id']
-                if cabin_id not in cabins_with_seats:
-                    cabins_with_seats[cabin_id] = {
-                        'Cabin': {
-                            'id': seat['cabin__id'],
-                            'location_id': seat['cabin__location_id'],
-                            'name': seat['cabin__name'],
-                            'code': seat['cabin__code'],
-                        },
-                        'Seats': [],
-                    }
-                cabins_with_seats[cabin_id]['Seats'].append({
-                    'id': seat['id'],
-                    'status': seat['status'],
-                    'cabin_id': seat['cabin_id'],
-                    'code': seat['code'],
-                    'isBooked': seat['id'] in booked_reservations,
-                    'isReserved': seat['status'] == 'RESERVED',
-                })
-
-            result = list(cabins_with_seats.values())
-
-            return Response({'message': 'Available seats fetched successfully', 'data': result}, status=200)
-
-        except Exception as error:
-            print('Error getting available seats:', error)
-            return Response({'error': 'Internal server error'}, status=500)
         
 class DashboardSeatsView(APIView):
     def get(self, request):
